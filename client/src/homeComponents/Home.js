d9ea19d8308fb6a6eb0265a8c5dfb2196e27c416
deepan311 <deepanraj3112002@gmail.com>
2024-01-25 15:41:18 +0530

    Main page structure

--- client/src/homeComponents/Home.js
+++ client/src/homeComponents/Home.js
@@ -4,11 +4,11 @@ function Home() {
   return (
     <div className="grid grid-cols-12">
       {/* Left Bar */}
-      <div className="col-span-3">Left Bar</div>
+      <div className="hidden md:block md:col-span-4 lg:col-span-3 bg-red-400">Left Bar</div>
       {/* Main Page */}
-      <div className="col-span-6">Main page</div>
+      <div className="col-span-12 md:col-span-8 lg:col-span-6 bg-gray-200">Main page</div>
       {/* Right Page */}
-      <div className="col-span-3">right Bar</div>
+      <div className="hidden lg:block md:col-span-4 lg:col-span-3 bg-green-300">right Bar</div>
     </div>
   );
 }
